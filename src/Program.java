12d24db06177013f7dee0ddeae80a362a453eb87
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-27 19:56:31 -0300

    Leitura do nome e salario

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in); 
+		double salario;
+		
+		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
+		System.out.print("Nome do cliente: ");
+		String nome = sc.nextLine();
+	
+		do {
+			System.out.print("Salario: ");
+			salario = sc.nextDouble();
+			if(salario < 0) {
+				System.out.println("O salario nao pode ser negativo. Tente novamente.");
+			}
+		}while(salario < 0);
+		
 	}
 
 }
