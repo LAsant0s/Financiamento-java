aa74a4cac5853cbf4c654d2e830fa7158e0e467d
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-27 22:06:11 -0300

    Processamento do menu

--- src/Program.java
+++ src/Program.java
@@ -8,7 +8,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		double salario, entrada, valorTotal, valorPrestacoes;
-		int prestacoes;
+		int prestacoes, opcao;
 
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.print("Nome do cliente: ");
@@ -39,6 +39,38 @@ public class Program {
 			}
 
 		} while (valorPrestacoes > salario * 0.3);
+		
+		System.out.println();
+		
+		do {
+			System.out.println("MENU:");
+			System.out.println("1 - Mostrar valor da entrada");
+			System.out.println("2 - Mostrar o valor financiado");
+			System.out.println("3 - Mostrar o valor de cada prestacao");
+			System.out.println("4 - Sair");
+			System.out.print("Digite uma opcao: ");
+			opcao = sc.nextInt();
+			System.out.println();
+			
+			switch(opcao) {
+				case 1:
+					System.out.printf("ENTRADA = R$ %.2f\n\n", valorTotal * entrada / 100);
+					break; 
+				case 2: 
+					System.out.printf("VALOR FINANCIADO = R$ %.2f\n\n", (valorTotal - (valorTotal * entrada) / 100));
+					break; 
+				case 3: 
+					System.out.printf("VALOR DE CADA PRESTACAO = R$ %.2f\n\n", valorPrestacoes);
+					break;
+				case 4:
+					System.out.println("FIM DO PROGRAMA!");
+					break;
+				default:
+					System.out.println("Valor inválido. tente novamente.\n");
+			}
+		}while(opcao != 4);
+		
+		
 	}
 
 }
